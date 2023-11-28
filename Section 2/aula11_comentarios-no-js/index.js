c39ac4423b1f0defc7400f425c75623dac5ae57f
msfukuji <msfukuji@gmail.com>
2023-11-28 18:33:34 -0300

    update aulas

---
+++ Section 2/aula11_comentarios-no-js/index.js
@@ -0,0 +1,11 @@
+// escrever comentários usando barra barra, curtos, para linhas.
+console.log('Olá mundo!'); /* Outro comentário com Ctrl+Shift+A para blocos*/
+console.log('Linha 1');
+/* Podemos utilizar para escrever várias linhas
+de comentários, podendo selecionar um bloco do 
+código para ser comentado e não executado. */
+console.log('Linha 2');
+// comentários
+console.log('Linha 3');
+/* console.log('Linha 4');
+console.log('Linha 5'); */
