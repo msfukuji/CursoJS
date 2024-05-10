f9801dc0b4d9e8a68abe3089f3c188dc623665b2
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-10 17:35:33 +0000

    aula13

--- S2 - JS Básico/aula12/index.js
+++ S2 - JS Básico/aula12/index.js
@@ -1,1 +1,4 @@
-console.log('Olá mundo!');
+console.log('Olá Mundo!');
+console.log('Este trecho será exibido no navegador');
+console.log('Lorem ipsum dolor sit amet consectetur adipisicing elit.');
+alert('Olá Mundo!') // Essa mensagem é Blocking, só prossegue após a confirmação pelo usuário.
---
+++ S2 - JS Básico/aula13/index.js
@@ -0,0 +1,10 @@
+// Variáveis:
+
+let nome = 'João';
+
+
+console.log('João nasce em 1984.');
+console.log('No ano 2000, João conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('Maria teve um filho com João em 2015.');
+console.log('O filho de João se chama Santiago.');
