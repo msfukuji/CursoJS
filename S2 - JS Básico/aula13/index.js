582e44b39a48ef2fa9474e7b9a77a56ebfabc049
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-10 18:21:03 +0000

    aula13 index.js

--- S2 - JS Básico/aula13/index.js
+++ S2 - JS Básico/aula13/index.js
@@ -1,10 +1,21 @@
-// Variáveis:
+/* 
+Variáveis: 
+Não podemos utilizar palavras reservadas para criar variáveis, e.g. if, let, console etc.
+Variáveis precisam ter nomes significativos, e.g. nomeCidade, idadeCliente, ano, nomeCliente
+Não podemos criar variáveis com nome iniciado por número, e.g. 3nergia.
+Não podem conter espaços ou traços, e.g. nome-Cliente, nome Cliente, assim utilizar a notação camelCase, e.g. nomeCompletoDoCliente, nomeCliente, 
+Case-sensitive, letra maiuscula e minuscula fazem diferença, e.g. nomeCliente e nomecliente são variáveis diferentes.
+*/
+/*let nome = 'João'; // String.
+console.log(nome, 'nasce em 1984.');
+console.log('No ano 2000,', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Santiago.');
+*/
 
-let nome = 'João';
-
-
-console.log('João nasce em 1984.');
-console.log('No ano 2000, João conheceu Maria.');
-console.log('João casou-se com Maria em 2012.');
-console.log('Maria teve um filho com João em 2015.');
-console.log('O filho de João se chama Santiago.');
+let nome; // Declarou a variável.
+nome = 'João'; // Inicializando a variável.
+console.log(nome);
+nome = 'Marcelo'
+console.log(nome);
