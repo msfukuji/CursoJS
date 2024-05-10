2601b3eaf5b7505a1a16d87c6d4eac983cdd8119
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-10 16:28:52 +0000

    aula12

---
+++ S2 - JS Básico/aula11/index.js
@@ -0,0 +1,13 @@
+// para escerever um cometário em uma linha. Atalho Ctrl+K+C
+console.log('Olá mundo'); // Aqui utem outro cometário
+//console.log('Linha 1');
+console.log('Linha 2');
+/* console.log('Linha 3');
+console.log('Linha 4');*/
+console.log('Linha 5');
+// vai ser ignorado
+
+/* Comentário de bloco de código
+Pode escrever várias linhas comentadas*/
+
+
