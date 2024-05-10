5d5033d429951168a42701f7d2edb512708739d8
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-10 14:37:52 +0000

    Aula7

--- S2 - JS Básico/aula7/index.js
+++ S2 - JS Básico/aula7/index.js
@@ -1,1 +1,6 @@
-console.log('Marcelo Fukuji');
+console.log('Marcelo "Fukuji"'); //string = texto
+console.log("Marcelo 'Fukuji'");
+console.log(`'Marcelo' "Fukuji"`);
+
+console.log(123456); // number = número
+console.log(3.141516);
