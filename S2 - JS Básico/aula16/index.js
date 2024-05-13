b68148bb413b03dfb05d322fbb097987e204b184
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-13 16:50:28 +0000

    aula16_1

--- S2 - JS Básico/aula16/index.js
+++ S2 - JS Básico/aula16/index.js
@@ -4,3 +4,7 @@ Marcelo Fukuji tem 40 anos, pesa 89kg, tem 1.78 de altura e seu IMC é de 27.5.
 const nome = 'Marcelo';
 const sobrenome = 'Fukuji';
 const idade = 40;
+const peso = 89;
+const altura = 1.78;
+const imc = peso/(altura*altura);
+console.log (nome, sobrenome, 'tem', idade, 'anos de idade, seu peso é de', peso, 'kg, tem de altura', altura, 'm e seu IMC é de', imc,'.');
