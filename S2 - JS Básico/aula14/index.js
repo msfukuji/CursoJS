bd0e427179f030ee9e06b09ff57474be79673790
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-13 16:29:36 +0000

    aula14

--- S2 - JS Básico/aula14/index.js
+++ S2 - JS Básico/aula14/index.js
@@ -1,11 +1,30 @@
 /* 
-Variáveis: 
-Não podemos utilizar palavras reservadas para criar variáveis, e.g. if, let, console etc.
-Variáveis precisam ter nomes significativos, e.g. nomeCidade, idadeCliente, ano, nomeCliente
-Não podemos criar variáveis com nome iniciado por número, e.g. 3nergia.
+Constantes: 
+Não podemos utilizar palavras reservadas para criar constantes, e.g. if, let, console etc.
+Constantes precisam ter nomes significativos, e.g. nomeCidade, idadeCliente, ano, nomeCliente
+Não podemos criar constantes com nome iniciado por número, e.g. 3nergia.
 Não podem conter espaços ou traços, e.g. nome-Cliente, nome Cliente, assim utilizar a notação camelCase, e.g. nomeCompletoDoCliente, nomeCliente, 
 Case-sensitive, letra maiuscula e minuscula fazem diferença, e.g. nomeCliente e nomecliente são variáveis diferentes.
-Não podemos redaclarar variáveis com let, e.g let nomeCliente = 'Marcelo' e depois redeclarar como let nomeCliente = 'João', mas pode mudar o valor: nomeCliente = 'João'.
-NÃO UTILIZE VAR, UTILIZE LET.
+Não pode modificar o valor de uma constante.
+NÃO UTILIZE VAR, UTILIZE CONST.
+SEMPRE UTILIZAR CONST PARA EVITAR A ALTERAÇÃO DO VALOR DA VARIÁVEL NO CÓDIGO, CASO NECESSÁRIO, ALTERAR PARA LET.
 */
 
+const nome = 'João';
+console.log(nome);
+
+// + - * /
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero + segundoNumero;
+const resultadoDuplicado = resultado * 2;
+const resultadoTriplicado = resultado * 3;
+console.log (resultado);
+console.log (resultadoDuplicado);
+console.log (resultadoTriplicado);
+
+// Para verificar o tipo do elemento utiliza typeof:
+const terceiroNumero = '5';
+console.log (typeof primeiroNumero);
+console.log (typeof terceiroNumero);
+console.log (terceiroNumero + segundoNumero);
