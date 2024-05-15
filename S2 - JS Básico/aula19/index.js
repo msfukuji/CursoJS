a820821d07de16262ad86142c34644859b96ce03
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-15 17:16:22 +0000

    aula19_1

--- S2 - JS Básico/aula19/index.js
+++ S2 - JS Básico/aula19/index.js
@@ -10,3 +10,12 @@ const num2 = 10.52; // number
 let nomeAluno; // undefined -> não aponta para nenhum local na memoória do JS. Variável declarada mas não inicializada
 const sobrenomeAluno = null; // Nulo -> não aponta para nenhum local na memoória do JS.
 const aprovado = true; // Valor Boolean (true or false)
+
+console.log(typeof nome, nome);
+console.log(typeof nome1, nome1);
+console.log(typeof nome2, nome2);
+console.log(typeof num1, num1);
+console.log(typeof num2, num2);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
