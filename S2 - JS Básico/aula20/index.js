492e51ab7f5fea856d975718c8edd4a194dfb582
Marcelo Fukuji <149309231+msfukuji@users.noreply.github.com>
2024-05-16 13:59:31 +0000

    aula20_2

--- S2 - JS Básico/aula20/index.js
+++ S2 - JS Básico/aula20/index.js
@@ -9,7 +9,8 @@ Aritiméticos:
 % Resto da divisão
 Precedência: () ** * / % + -
 */
-const num1 = 10;
+
+/*const num1 = 10;
 const num2 = 5;
 const num3 = '5';
 console.log(num1 + num2);
@@ -20,8 +21,17 @@ console.log(num1 / num2);
 console.log(num1 ** num2);
 console.log(num1 % num2);
 console.log((num1 + num2) * num3);
-
+console.log('contador');
 let contador = 1
 contador++; // 2 -> Não pode usar o operador ++ com const, pois const não deixa mudar o valor
-contador++; // 3
+contador++; // 3 -> Melhor prática é fazer o incremento ou decremento depois chamar o console.log
+console.log(contador);
+*/
+//Qdo der erro NaN -> Not a Number
+// Operadores de atribuição:
+let contador = 0;
+contador += 2;
+contador *= 2;
+console.log(contador);
+contador **=10;
 console.log(contador);
